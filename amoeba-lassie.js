60c75d5b51a7586ddde9b31a84edb19b17ee2a1b
Milos Knezevic <ushtipak@gmail.com>
2019-08-26 00:02:52 +0200

    Refactor a bit more :)

--- amoeba-lassie.js
+++ amoeba-lassie.js
@@ -1,7 +1,8 @@
 class AmoebaLassie extends Phaser.Scene {
     constructor() {
         super({key: "AmoebaLassie"});
-
+        health = 5;
+        killed = 0;
     }
 
     preload() {
@@ -14,14 +15,10 @@ class AmoebaLassie extends Phaser.Scene {
 
         amoebas = this.physics.add.group();
         let amoeba = amoebas.create(Phaser.Math.Between(0, 600), Phaser.Math.Between(0, 400), 'amoeba-lassie').setInteractive();
-        amoeba.setBounce(1);
-        amoeba.setCollideWorldBounds(true);
-        amoeba.setVelocity(
-            Phaser.Math.Between(-400, 400),
-            Phaser.Math.Between(-400, 400)
-        );
+        spawn(amoeba);
+
         amoeba.on('pointerdown', function (pointer) {
-            hitAmoeba(this)
+            hit(this)
         });
         amoeba.on('pointerup', function (pointer) {
             this.clearTint();
@@ -32,5 +29,28 @@ class AmoebaLassie extends Phaser.Scene {
 
         killedText = this.add.text(20, 20, '> 0 amoebas killed', {fontSize: '24px', fill: '#000'});
     }
-
 }
+
+function spawn(amoeba) {
+    amoeba.enableBody(true, Phaser.Math.Between(0, 600), Phaser.Math.Between(0, 400), true, true);
+    amoeba.setBounce(1);
+    amoeba.setCollideWorldBounds(true);
+    amoeba.setVelocity(
+        Phaser.Math.Between(-400, 400),
+        Phaser.Math.Between(-400, 400)
+    );
+    health = 5;
+}
+
+function hit(amoeba) {
+    amoeba.setTint(0xff0000);
+    health -= 1;
+    console.log(health);
+    if (health === 0) {
+        amoeba.disableBody(true, true);
+        killed += 1;
+        killedText.setText('> ' + killed + ' amoebas killed');
+        spawn(amoeba);
+    }
+}
+

f84e13cf517241ec69f80717bfaa1f2d5fb3a40e
Milos Knezevic <ushtipak@gmail.com>
2019-08-27 18:32:55 +0200

    Add optional Lassie complain about golang (in ~14%  of cases :)

--- amoeba-lassie.js
+++ amoeba-lassie.js
@@ -27,6 +27,7 @@ class AmoebaLassie extends Phaser.Scene {
         this.load.image('java-rant-7', 'assets/java-rant-7.png');
         this.load.image('java-rant-8', 'assets/java-rant-8.png');
         this.load.image('java-rant-9', 'assets/java-rant-9.png');
+        this.load.audio('golang', 'assets/golang.mp3');
         this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
     }
 
@@ -37,11 +38,15 @@ class AmoebaLassie extends Phaser.Scene {
         amoebas = this.physics.add.group();
         hackles = this.physics.add.group();
 
+        let complain = this.sound.add("golang", {loop: false});
         let amoeba = amoebas.create(Phaser.Math.Between(...amoebaPosX), Phaser.Math.Between(...amoebaPosY), 'amoeba-lassie').setInteractive();
         spawn(amoeba);
 
         amoeba.on('pointerdown', function (pointer) {
             hit(this);
+            if (Phaser.Math.Between(1, 7) === 3) {
+                complain.play();
+            }
             let hackle = hackles.create(pointer.x, pointer.y, "java-rant-".concat(Phaser.Math.Between(1, 9)));
             hackle.setVelocity(Phaser.Math.Between(...hackleVelX), Phaser.Math.Between(...hackleVelY));
         });
